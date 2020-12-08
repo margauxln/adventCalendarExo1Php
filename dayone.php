aa5c79b7b92c86b880ea989de39e629dd8c7ab98
Margaux Lenne <margaux.lennebonety@gmail.com>
2020-12-08 14:09:18 +0100

    fin exo

--- dayone.php
+++ dayone.php
@@ -1,8 +1,14 @@
 <?php 
 $arrayOfInput =file ("input.txt");
-print_r ($arrayOfInput);
-foreach ($arrayOfInput as $key => $value){
-    echo $key . "      ";
-    echo $value . "    <br>";
-}
+foreach ($arrayOfInput as $key => $nombre1){
+    foreach ($arrayOfInput as $key2 => $nombre2){
+        $result=(intval($nombre1) + intval($nombre2));
+        if ($result === 2020){
+            echo "<li>"."$nombre1" . " et " . "$nombre2 <br>";
+            echo "</li>";
+            echo "answer = ". intval($nombre1) * intval($nombre2);
+            return;
+        }
+    }
+} 
 ?>
